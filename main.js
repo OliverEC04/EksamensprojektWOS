a13ee9eb9a5efd688014f021c9f44d6b729384ab
OliverEC04 <oliverec04@gmail.com>
2020-04-29 12:15:12 +0200

    img
    Co-authored-by: WilliamT_HTX <wtrojel@gmail.com>
    Co-authored-by: simon@thule.dk <simon@thule.dk>

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 ///<reference path="p5.global-mode.d.ts" />
+const canvasSize =
+{
+    x: 1000,
+    y: 800
+};
 
+function setup()
+{
+    createCanvas(canvasSize.x, canvasSize.y);
+
+}
+
+function draw()
+{
+    background(0, 0, 0);
+
+}
--- noter.md
+++ noter.md
@@ -1,4 +1,3 @@
 # Noter
-* Sliders
-* Knapper
-* Reset
+* Ting der skal med: Økonomi, mental helbred, helbred, udgangsforbud (rejse eller gå ud af huset), beskeder om ting der sker i samfundet(hospitalt brændt, mette f er til børn)
+* 
