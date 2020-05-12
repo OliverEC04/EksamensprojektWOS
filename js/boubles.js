3f7f3144044f8e6b29476170989583f90016fa59
OliverEC04 <oliverec04@gmail.com>
2020-05-12 22:52:40 +0200

    mange prikker

--- js/boubles.js
+++ js/boubles.js
@@ -11,6 +11,7 @@ class Boubles
         this.deadRadius = this.country.dead / this.country.population * 4000;
 
         fill(255, 0, 0);
+        strokeWeight(2);
         circle(this.country.mapPosition.x, this.country.mapPosition.y, this.infectedRadius * 2);
         fill(0);
         circle(this.country.mapPosition.x, this.country.mapPosition.y, this.deadRadius * 2);
@@ -26,8 +27,8 @@ class Boubles
         this.fontSize = 15;
         this.panelSize =
         {
-            x: 120,
-            y: 80
+            x: 200,
+            y: 60
         };
 
         fill(255)
@@ -36,6 +37,10 @@ class Boubles
         fill(0);
         textSize(this.fontSize);
         text(this.country.name, this.country.mapPosition.x + this.infectedRadius * 2 + this.fontSize * .5, this.country.mapPosition.y - this.panelSize.y * .5 + this.fontSize);
+        strokeWeight(0);
+        text("Population: " + this.country.population, this.country.mapPosition.x + this.infectedRadius * 2 + this.fontSize * .5, this.country.mapPosition.y - this.panelSize.y * .3 + this.fontSize);
+        text("Infected: " + this.country.infected, this.country.mapPosition.x + this.infectedRadius * 2 + this.fontSize * .5, this.country.mapPosition.y - this.panelSize.y * .1 + this.fontSize);
+        text("Dead: " + this.country.dead, this.country.mapPosition.x + this.infectedRadius * 2 + this.fontSize * .5, this.country.mapPosition.y - this.panelSize.y * (-.1) + this.fontSize);
     }
 }
 
--- js/data.js
+++ js/data.js
@@ -1,17 +1,150 @@
-var countries = 
+﻿var countries = 
 [
     {
-        name: "Denmark",
+        name: "Danmark",
         mapPosition: {x: mapPos.x + mapWidth * .32, y: mapPos.y + mapWidth * .5},
         population: 5600000,
         infected: 9311,
         dead: 452
     },
     {
-        name: "Sweden",
+        name: "Sverige",
         mapPosition: {x: mapPos.x + mapWidth * .4, y: mapPos.y + mapWidth * .3},
         population: 7000000,
         infected: 11000,
         dead: 900
-    }
+    },
+    {
+        name: "Norge",
+        mapPosition: {x: mapPos.x + mapWidth * .33, y: mapPos.y + mapWidth * .4},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Tyskland",
+        mapPosition: {x: mapPos.x + mapWidth * .34, y: mapPos.y + mapWidth * .6},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Island",
+        mapPosition: {x: mapPos.x + mapWidth * .06, y: mapPos.y + mapWidth * .23},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Finland",
+        mapPosition: {x: mapPos.x + mapWidth * .5, y: mapPos.y + mapWidth * .3},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Rusland",
+        mapPosition: {x: mapPos.x + mapWidth * .8, y: mapPos.y + mapWidth * .4},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Storbritanien",
+        mapPosition: {x: mapPos.x + mapWidth * .18, y: mapPos.y + mapWidth * .55},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Irland",
+        mapPosition: {x: mapPos.x + mapWidth * .1, y: mapPos.y + mapWidth * .55},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Holland",
+        mapPosition: {x: mapPos.x + mapWidth * .28, y: mapPos.y + mapWidth * .59},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Belgien",
+        mapPosition: {x: mapPos.x + mapWidth * .25, y: mapPos.y + mapWidth * .63},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Luxemborg",
+        mapPosition: {x: mapPos.x + mapWidth * .28, y: mapPos.y + mapWidth * .64},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Frankrig",
+        mapPosition: {x: mapPos.x + mapWidth * .22, y: mapPos.y + mapWidth * .7},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Spanien",
+        mapPosition: {x: mapPos.x + mapWidth * .1, y: mapPos.y + mapWidth * .85},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Portugal",
+        mapPosition: {x: mapPos.x + mapWidth * .02, y: mapPos.y + mapWidth * .85},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Schweiz",
+        mapPosition: {x: mapPos.x + mapWidth * .3, y: mapPos.y + mapWidth * .71},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Italien",
+        mapPosition: {x: mapPos.x + mapWidth * .44, y: mapPos.y + mapWidth * .85},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Vatikanstaten",
+        mapPosition: {x: mapPos.x + mapWidth * .38, y: mapPos.y + mapWidth * .8},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "San Marino",
+        mapPosition: {x: mapPos.x + mapWidth * .37, y: mapPos.y + mapWidth * .78},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Liechtenstein",
+        mapPosition: {x: mapPos.x + mapWidth * .33, y: mapPos.y + mapWidth * .71},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
+    {
+        name: "Østrig",
+        mapPosition: {x: mapPos.x + mapWidth * .4, y: mapPos.y + mapWidth * .7},
+        population: 5600000,
+        infected: 9311,
+        dead: 452
+    },
 ];
